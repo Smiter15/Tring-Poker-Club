ea98eaec06df13d0bfb1e35be90fb7e6596fb4e8
Matt <matt@mattslibrary.co.uk>
2026-04-16 11:00:54 +0100

    try remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: netlify()
 });
